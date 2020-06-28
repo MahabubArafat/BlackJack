d332002cdf06dd9ab5e09c2930a2442fa8d0fc83
MahabubArafat <arafatmahabub37@gmail.com>
2020-06-28 07:45:28 +0600

    added_human_ace_value_selection

--- black_jack.py
+++ black_jack.py
@@ -3,7 +3,7 @@ Black jack Black jack
 """
 game = True
 hitter = 0
-com_game = True
+com_game = main_game = True
 winner: str = ' '
 bet = 201
 
@@ -17,7 +17,7 @@ class Bet():
     def place_bet(self):
         global bet
         while True:
-            print("\r how much bet you want to place :", end=" ")
+            print("\r>>>>>>>>>>>> how much bet you want to place :", end=" ")
             bet = int(input())
             if bet <= self.human_bankroll:
                 break
@@ -35,7 +35,15 @@ class Bet():
                 self.human_bankroll = self.human_bankroll + (2 * bet)
                 print(f"new balance : ${self.human_bankroll}")
         except:
-            print("no clear winner,play again and bet bank roll is the same")
+            print("no clear winner,play again and bet, bank roll is the same")
+
+    def bankroll_check(self):
+        global main_game
+        if self.human_bankroll <= 0:
+            print("You can't Play anymore, You loose")
+            print("Game Finished, Exiting Now")
+            main_game = False
+            # return main_game
 
 
 def deck_creation():
@@ -53,7 +61,7 @@ def deck_creation():
     for mark in marks:
         for num in numbers:
             card_deck.append([mark, num, values[num]])
-
+    print(">Deck Created")
     return card_deck
 
 
@@ -75,7 +83,12 @@ def shuffling():
     :return:
     """
     import random
-    random.shuffle(deck)
+    try:
+        print(">>shuffling cards..............")
+        print(">>>card shuffled")
+        random.shuffle(deck)
+    except:
+        print("restart the game")
 
 
 non_repeat = []
@@ -106,10 +119,18 @@ def hit_stay(card_value):
         response = input()
         if response.lower() == 'y':
             printing_card(non_repeat[hitter])
+            if deck[non_repeat[hitter]][1] == 'ace':
+                print(
+                    "\rYou have an ace, Enter \"y\" to make it 1 or n to let it be 11 :", end=" ")
+                ace_decider = input()
+                if ace_decider.lower() == 'y':
+                    deck[non_repeat[hitter]][2] = 1
+                else:
+                    pass
             card_value = card_value + deck[non_repeat[hitter]][2]
             human_card_value = card_value  # returning the human_card_value
             if card_value > 21:
-                print(card_value)
+                print(f"Human card value : {card_value}")
                 print("Busted com won")
                 winner = 'com'
                 hitter += 1
@@ -118,19 +139,20 @@ def hit_stay(card_value):
                 break
             elif card_value == 21:
                 print(card_value)
-                print("Computer\'s turn to hit")
+                print(f"Human card value : {card_value}")
                 hitter += 1
                 break
             elif card_value < 21:
-                print(card_value)
+                print(f"Human card value : {card_value}")
                 hitter += 1
                 continue
         else:
             print("so you are staying and Now Computer's turn")
             break
 
+
 lets_bet = Bet()
-while True:
+while main_game:
     while game:
         deck = deck_creation()
         shuffling()
@@ -151,11 +173,11 @@ while True:
             if human_card_value == com_card_value == 21:
                 print("both hit black jack,so draw")
                 game = False
-                com_game=False
+                com_game = False
             else:
                 print("Human has won the game")
                 winner = 'human'
-                com_game=False
+                com_game = False
                 game = False
         else:
             hit_stay(human_card_value)
@@ -183,17 +205,19 @@ while True:
                 game = False
                 break
 
-        lets_bet.selecting_winner(winner)
-
-    print("\r wana play again ? type y to play again and type n to exit:",end=" ")
-    response=input()
-    if response.lower()=='y':
-        com_game=True
-        game=True
-        continue
-    elif response.lower()=='n':
-        break
+    lets_bet.selecting_winner(winner)
+    lets_bet.bankroll_check()
+    if main_game == True:
+        print("\r wana play again ? type y to play again and type n to exit:", end=" ")
+        response = input()
+        if response.lower() == 'y':
+            com_game = True
+            game = True
+            continue
+        elif response.lower() == 'n':
+            break
+        else:
+            print("No clear response you moron, exiting the game..........")
+            break
     else:
-        print("No clear response you moron, exiting the game...")
         break
-
